08ff0c0193f1eadeceede28436719b9f6b497d82
onetoughcookie226 <onetoughcookie226@gmail.com>
2016-06-16 12:09:19 +0200

    inital commi

---
+++ storymaking.js
@@ -0,0 +1,23 @@
+/**
+ * Created by vagrant on 6/15/16.
+ */
+// The story making game
+//Create a variable and capture input
+    var noun = prompt ("what do you like to eat now?");
+    var verb = prompt ("what did you do this morning?");
+    var adj = prompt ("How do you feel now?");
+
+    alert("Well done !! Your story is ready");
+
+    var story = "Last night my " +  adj +  " cat was in the kitchen " +
+        verb + "ing" + " . Today, I had " + noun + " for lunch.";
+
+    document.write (story);
+
+//Create a separate variable for each piece of input
+
+//Add an alert to tell the visitors they are finished 
+
+// Combine the input with other strings to create a message
+
+// Print the story to the browser window
