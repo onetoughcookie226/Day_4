f126b2686687185aa91f95fc7f5501af8bb1aed1
onetoughcookie226 <onetoughcookie226@gmail.com>
2016-06-24 19:40:35 +0200

    create a question remaining countdown

--- storymaking.js
+++ storymaking.js
@@ -2,22 +2,34 @@
  * Created by vagrant on 6/15/16.
  */
 // The story making game
+    
 //Create a variable and capture input
-    var noun = prompt ("what do you like to eat now?");
-    var verb = prompt ("what did you do this morning?");
-    var adj = prompt ("How do you feel now?");
-
+    //Create a separate variable for each piece of input
+    //Let the player know how many questions are left to be answered.
+    var questions = 3;
+    var questionsLeft = '[there are ' + questions + ' questions left.]';
+    var noun = prompt ("what do you like to eat now?" + questionsLeft);
+    questions -= 1;
+    var questionsLeft = '[there are ' + questions + ' questions left.]';
+    var verb = prompt ("what did you do this morning?" +  questionsLeft);
+    questions -= 1;
+    var questionsLeft = '[there are ' + questions + ' questions left.]';
+    var adj = prompt ("How do you feel now?" +  questionsLeft));
+//Add an alert to tell the visitors they are finished 
     alert("Well done !! Your story is ready");
-
+// Combine the input with other strings to create a message
     var story = "Last night my " +  adj +  " cat was in the kitchen " +
         verb + "ing" + " . Today, I had " + noun + " for lunch.";
-
+// Print the story to the browser window
     document.write (story);
 
-//Create a separate variable for each piece of input
+//Time calculator
 
-//Add an alert to tell the visitors they are finished 
 
-// Combine the input with other strings to create a message
 
-// Print the story to the browser window
+
+
+
+
+
+
